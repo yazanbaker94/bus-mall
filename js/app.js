e523744e0c3aff11a54e980bce6614c376628986
yazanbaker94 <79137633+yazanbaker94@users.noreply.github.com>
2021-03-15 15:47:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -47,12 +47,14 @@ function randomNumber(min, max) {
 function RandomItems(name, ext){
     this.name = name;
     this.path = `./img/${name}.${ext}`;
-    this.visits = 0;
+    this.clicks = 0;
+    this.views = 0;
     RandomItems.all.push(this);
 
 }
 
 RandomItems.all = [];
+
 
 for(let i=0; i<randomList.length; i++) {
     
@@ -66,70 +68,93 @@ for(let i=0; i<randomList.length; i++) {
 }
 }
 
-let listArray = [];
+
 
 function render() {
-    let leftIndex = randomNumber(0, randomList.length-1);
+
+    let rightIndex;
+    let leftIndex;
+    let middleIndex;
+    
+    do { rightIndex = randomNumber(0, randomList.length-1);
+         leftIndex = randomNumber(0, randomList.length-1);
+         middleIndex = randomNumber(0, randomList.length-1 );
+    }
+
+    while(leftIndex === middleIndex || leftIndex === rightIndex || middleIndex === rightIndex);
+      
+   
+    
     let randomLeftImage = RandomItems.all[leftIndex];
     leftImage.src = randomLeftImage.path;
     leftImage.title = randomLeftImage.name;
     leftImage.alt = randomLeftImage.name;
-    
+    RandomItems.all[leftIndex].views++;
  
-      
-        
- 
-
-
- 
-    
-
-    let middleIndex = randomNumber(0, randomList.length-1);
     let randomMiddleImage = RandomItems.all[middleIndex];
     middleImage.src = randomMiddleImage.path;
     middleImage.title = randomMiddleImage.name;
     middleImage.alt = randomMiddleImage.name;
+    RandomItems.all[middleIndex].views++;
 
-   
-
-    let rightIndex = randomNumber(0, randomList.length-1);
+    
     let randomRightImage = RandomItems.all[rightIndex];
     rightImage.src = randomRightImage.path;
     rightImage.title = randomRightImage.name;
     rightImage.alt = randomRightImage.name;
-
+    RandomItems.all[rightIndex].views++;
+    
+    console.log(RandomItems.all[15]);
     
 
+    } 
 
-}
+let buttonSection = document.getElementById('buttonSection');
 
 
 imageSection.addEventListener('click', clickHandler);
 
-
+let maxClicks = 25;
 function clickHandler(event) {
+    maxClicks-=1;
     if(event.target.id === 'leftImage' || event.target.id === 'rightImage' || event.target.id === 'middleImage'){
         for(let i=0; i<RandomItems.all.length; i++) {
             if(RandomItems.all[i].name === event.target.title) {
-                RandomItems.all[i].visits++;
-                console.log(RandomItems.all[i].name + ' ' + RandomItems.all[i].visits);
+                RandomItems.all[i].clicks++;
+               
             }
 
         }
         render();
+       
+    }
+    if(maxClicks === 0){
+    imageSection.removeEventListener('click', clickHandler);
+    let resultButton = document.createElement('button');
+    resultButton.textContent = 'View Results';
+    buttonSection.appendChild(resultButton);
 
+    resultButton.addEventListener('click', buttonClickHandler);
+
+    function buttonClickHandler(event) {
+        let unorderedList = document.createElement('ul');
+        buttonSection.appendChild(unorderedList);
+        
+        
+
+        for(let i=0; i<RandomItems.all.length; i++) {
+            let listItems = document.createElement('li');
+            unorderedList.appendChild(listItems);
+            listItems.textContent = RandomItems.all[i].name + " had " + RandomItems.all[i].clicks + " votes and views are " + RandomItems.all[i].views;
+        }
     }
 
 
+        
+    }
+  
 
 }
-
-
-
-
- 
-
-
 
 
 
