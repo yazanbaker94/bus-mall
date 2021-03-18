b4cad0de89e37c40bf3287c0c7abba29f5a0f0c0
yazanbaker94 <79137633+yazanbaker94@users.noreply.github.com>
2021-03-18 20:39:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-
+// randomList array
 let randomList = [
     'bag',
     'banana',
@@ -24,26 +24,26 @@ let randomList = [
     'water-can',
     'wine-glass'
 ];
-
+// make extensions for the checker
 let ext = [
     'jpg',
     'png',
     'gif'
 ];
 
-
+// got the ID's of the images and section
 let leftImage = document.getElementById('leftImage');
 let middleImage = document.getElementById('middleImage');
 let rightImage = document.getElementById('rightImage');
 let imageSection = document.getElementById('imageSection');
 
-
+// create a random function to be used later on to generate the Items.
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 
-
+// function to define the name, path, clicks, views, and then push it to the array
 function RandomItems(name, ext){
     this.name = name;
     this.path = `./img/${name}.${ext}`;
@@ -54,10 +54,10 @@ function RandomItems(name, ext){
 
 
 }
-
+// make randomItems array
 RandomItems.all = [];
 
-
+// create an exception for usb and sweep, else it continues normally for the extensions
 for(let i=0; i<randomList.length; i++) {
     
     if(randomList[i] === 'usb'){
@@ -70,9 +70,9 @@ for(let i=0; i<randomList.length; i++) {
 }
 }
 
-
+// create a duplicate array to make sure the items in the array to not duplicate
 let duplicatedArray = [];
-
+// render function to eventually show the images on the website and make sure they are not duplicated on the first round or there aren't any similiraties with the second round
 function render() {
 
     let rightIndex;
@@ -83,7 +83,7 @@ function render() {
         leftIndex = randomNumber(0, randomList.length-1);
         
         middleIndex = randomNumber(0, randomList.length-1 );
-        
+     // resolving duplication statement inside a while loop   
     }while(leftIndex === middleIndex || leftIndex === rightIndex || middleIndex === rightIndex || duplicatedArray.includes(rightIndex) || duplicatedArray.includes(leftIndex) || duplicatedArray.includes(middleIndex));
     duplicatedArray[0] = rightIndex;
     
@@ -92,20 +92,22 @@ function render() {
     
     duplicatedArray[2] = middleIndex;
    
-    
+    // generate random left IMage
     let randomLeftImage = RandomItems.all[leftIndex];
     leftImage.src = randomLeftImage.path;
     leftImage.title = randomLeftImage.name;
     leftImage.alt = randomLeftImage.name;
     RandomItems.all[leftIndex].views++;
- 
+     // generate random middle IMage
+
     let randomMiddleImage = RandomItems.all[middleIndex];
     middleImage.src = randomMiddleImage.path;
     middleImage.title = randomMiddleImage.name;
     middleImage.alt = randomMiddleImage.name;
     RandomItems.all[middleIndex].views++;
 
-    
+        // generate random right IMage
+
     let randomRightImage = RandomItems.all[rightIndex];
     rightImage.src = randomRightImage.path;
     rightImage.title = randomRightImage.name;
@@ -117,13 +119,14 @@ function render() {
 
     } 
 
-    
+    // get button ID
 let buttonSection = document.getElementById('buttonSection');
 
-
+// button click event listener
 imageSection.addEventListener('click', clickHandler);
 
-let maxClicks = 5;
+// after 25 clicks, the click button is disabled.
+let maxClicks = 25;
 function clickHandler(event) {
     maxClicks-=1;
     if(event.target.id === 'leftImage' || event.target.id === 'rightImage' || event.target.id === 'middleImage'){
@@ -151,12 +154,12 @@ function clickHandler(event) {
     }
   
 
-}
+}//view results button to be defined
 let resultButton = document.createElement('button');
         resultButton.textContent = 'View Results';
         buttonSection.appendChild(resultButton);
 
-
+// if view results button is clicked, graph is drawn
 function buttonClickHandler(event) {
     createChart();
     }
@@ -170,7 +173,7 @@ function randomNumber(min, max) {
   
 
 
-  
+  //chart creation
   function createChart(){
     let context = document.getElementById('myChart').getContext('2d');
     let getRandomNames=[];
@@ -179,7 +182,7 @@ function randomNumber(min, max) {
     let randomImages=getData();
     console.log(randomImages)
   
-
+//randomImages gets the data from the getData function, which is stored on the local storage
     for(let i=0;i<RandomItems.all.length;i++){
         getRandomViews.push(randomImages[i].views);
     }
@@ -190,7 +193,7 @@ function randomNumber(min, max) {
         getRandomClicks.push(randomImages[i].clicks);
     }
 
-
+// chartOBJECT includes Random image names and the random image clicks and random image views, from the local storage
     let chartObject={
         // The type of chart we want to create
         type: 'bar',
@@ -222,13 +225,13 @@ function randomNumber(min, max) {
     
 
       
-    
+    // render after view results is clicked
     // render();
     let chart = new Chart(context,chartObject);
    
     
 }
-
+// gets the data from the imagesList saved on the local storage
 function getData(){
     let data=localStorage.getItem('ImagesList');
     data=JSON.parse(data);
